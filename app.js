37e8d28fbbe2cee41d1d14683f8277a033667548
cloudstrife9494 <aymansmimah@yahoo.fr>
2017-12-04 12:37:55 +0100

    adding token configuration and verifying if it is passed as an argument to the program using yargs node module

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
 #! /usr/bin/env node
 
+var yargs = require('yargs');
+
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RtmClient = require('@slack/client').RtmClient;
 
 
-var token = 'xoxp-279576347152-280233591538-280320322114-cc87930d23883fa47792da284bc84149';
-let channel ;
-
-var rtmClient = new RtmClient(token);
+console.log(yargs.argv);
 
 
-rtmClient.on(CLIENT_EVENTS.RTM.AUTHENTICATED,(rtmDataStart) => {
-    console.log(JSON.stringify(rtmDataStart));
-    channel = rtmDataStart.channels[1].id;
-});
+
+var config = {
+    TOKEN : yargs.argv.token ,
+    MY_USERNAME : null,
+    CHANNEL :null
+}
 
 
-rtmClient.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED,() => {
-    console.log(channel);
-    rtmClient.sendMessage('hello from nodejs client',channel);
-});
+if(config.TOKEN) {
 
-rtmClient.start();
+
+    var rtmClient = new RtmClient(config.TOKEN);
+    //console.log(rtmClient);
+
+
+
+    rtmClient.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmDataStart) => {
+
+        //console.log(JSON.stringify(rtmDataStart));
+        config.CHANNEL = rtmDataStart.channels[1].id;
+    });
+
+
+
+
+
+    rtmClient.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
+        console.log(config.CHANNEL);
+        //rtmClient.sendMessage('hello from nodejs client', channel);
+    });
+
+    rtmClient.start();
+}
+
+else {
+    const message = "HELP[!]\n--token : set your slack token, to get it go ahead to " +
+        "(https://api.slack.com/docs/oauth-test-tokens) and generate one using your team slack account";
+
+    console.log(message);
+    process.exit();
+}
